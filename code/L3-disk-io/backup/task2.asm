2adfb6bd4ca8e7a670adf4658364202f3113b920
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2023-11-21 12:57:38 +0200

    HELL YEAH! 2.1 Seems to work

--- code/L3-disk-io/backup/task2.asm
+++ code/L3-disk-io/backup/task2.asm
@@ -1,106 +1,54 @@
-org 7c00h
-
-section .data
-    opt_str             dd "PICK AN OPTION:1. KBD ---> FLP2. FLP ---> RAM3. RAM ---> FLP"
-    opt_str_len         equ 15
-
-    in_await_str        dd "STRING = "
-    in_await_str_len    equ 9
-
-    in_await_n          dd "N = "
-    in_await_n_len      equ 4
-
-    in_await_hts        dd "{H, T, S} = "
-    in_await_hts_len    equ 12
-
-    test_string         dd "abcdefghiklmnop"
-    test_string_len     equ 15
-
-section .bss
-    string          resb 256
-    address         resb 9
-    num_in_buffer   resb 10
-    n               resb 4
-    head            resb 4
-    track           resb 4
-    sector          resb 4
-    buffer_size     resb 1
+org 1000h
 
 section .text
     global _start
 
 _start:
-    mov     si, opt_str
-    mov     cx, 5
-    push    cx
+    ; print options listing string
 
     mov     ah, 03h
     mov     bh, 0
     int     10h
 
-    mov     ah, 02h
-    inc     dh
-    mov     dl, 0
+    mov     ax, 0
+    mov     es, ax
+    mov     bp, opt_str
+
+    mov     bl, 07h
+    mov     cx, opt_len
+
+    mov     ax, 1301h
     int     10h
 
-    print_opt_list:
-        pop     cx
-        dec     cx
-        jz      read_option
+    mov     ah, 0eh
+    mov     al, 3ah
+    int     10h
 
-        push    cx
-        mov     cx, opt_str_len
+    mov     al, 20h
+    int     10h
 
-        print_line:
-            mov     ah, 0eh
-            mov     al, [si]
-            int     10h
+    ; read user's choice
 
-            inc     si
-            dec     cx
+    mov     ah, 00h
+    int     16h
 
-            cmp     cx, 0
-            jnz     print_line
+    ; execute chosen operation
 
-            mov     ah, 03h
-            mov     bh, 0
-            int     10h
+    cmp     al, '1'
+    je      option1
 
-            mov     ah, 02h
-            inc     dh
-            mov     dl, 0
-            int     10h
-
-            jmp     print_opt_list
-
-    read_option:
-        mov     ah, 03h
-        mov     bh, 0
-        int     10h
-
-        mov     ah, 02h
-        inc     dh
-        mov     dl, 0
-        int     10h
-
-        mov     ah, 00h
-        int     16h
-
-        direct_exec_flow:
-            cmp     al, '1'
-            je      option1
-
-            jmp     _error
+    jmp     _error
 
 option1:
+
+    ; display the key read
+
     mov     ah, 0eh
     int     10h
 
     mov     al, 2eh
     int     10h
 
-from_kbd_to_flp:
-    
     ; print "STRING = "
 
     mov     ah, 03h
@@ -112,23 +60,34 @@ from_kbd_to_flp:
 
     mov     ax, 0
     mov     es, ax
-    mov     bp, in_await_str
+    mov     bp, in_awaits_str
 
     mov     bl, 07h
-    mov     cx, in_await_str_len
+    mov     cx, in_await_len1
 
     mov     ax, 1301h
     int     10h
 
-    ; read the string
+    ; read user input (str)
 
+    call    read_in
+    ; call    print_in_buff
+
+    ; save the string to its own buffer
+
+    mov     si, in_buffer
     mov     di, string
-    mov     si, string
-    mov     byte [buffer_size], 255
-    call    read_input
-    call    print_read_debug
 
-    ; print "N ="
+    char_copy_loop:
+        mov     al, [si]
+        mov     [di], al
+        inc     si
+        inc     di
+
+        cmp     byte [si], 0
+        jne     char_copy_loop
+
+    ; print "N = "
 
     mov     ah, 03h
     mov     bh, 0
@@ -136,30 +95,31 @@ from_kbd_to_flp:
 
     inc     dh
     mov     dl, 0
- 
-	mov     ax, 0
-    mov     es, ax
-    mov     bp, in_await_n
 
-	mov     bl, 07h
-    mov     cx, in_await_n_len
+    mov     ax, 0
+    mov     es, ax
+    mov     si, in_awaits_str
+    add     si, in_await_len1
+    mov     bp, si
+
+    mov     bl, 07h
+    mov     cx, in_await_len2
 
     mov     ax, 1301h
     int     10h
 
-    ; read the n
+    ; read user input (n)
 
-    mov     di, num_in_buffer
-    mov     si, num_in_buffer
-    mov     byte [buffer_size], 9
-    call    read_input
-    call    print_read_debug
+    call    read_in
+    call    print_in_buff
 
-    mov     si, num_in_buffer
-    mov     di, n
+    ; convert ascii read to an integer
+
+    mov     di, nhts
+    mov     si, in_buffer
     call    atoi
 
-    ; check the result
+    ; print "{H, T, S} (one value per line):"
 
     mov     ah, 03h
     mov     bh, 0
@@ -167,37 +127,113 @@ from_kbd_to_flp:
 
     inc     dh
     mov     dl, 0
- 
-	mov     ax, 0
-    mov     es, ax
-    mov     bp, test_string
 
-	mov     bl, 07h
-    mov     cx, test_string_len
-    mov     ax, [n]
-    sub     cx, ax
+    mov     ax, 0
+    mov     es, ax
+    mov     si, in_awaits_str
+    add     si, in_await_len1
+    add     si, in_await_len2
+    mov     bp, si
+
+    mov     bl, 07h
+    mov     cx, in_await_len3
 
     mov     ax, 1301h
     int     10h
+
+    mov     ah, 0eh
+    mov     al, 3ah
+    int     10h
+
+    mov     ah, 02h
+    inc     dh
+    int     10h
+
+    ; read user input (h)
+
+    call    read_in
+    call    print_in_buff
+
+    mov     ah, 02h
+    inc     dh
+    mov     dl, 0
+    int     10h
+
+    ; convert ascii read to an integer
+
+    mov     di, nhts + 2
+    mov     si, in_buffer
+    call    atoi
+
+    ; read user input (t)
+
+    call    read_in
+    call    print_in_buff
+
+    mov     ah, 02h
+    inc     dh
+    mov     dl, 0
+    int     10h
+
+    ; convert ascii read to an integer
+
+    mov     di, nhts + 4
+    mov     si, in_buffer
+    call    atoi
+
+    ; read user input (s)
+
+    call    read_in
+    call    print_in_buff
+
+    mov     ah, 02h
+    inc     dh
+    mov     dl, 0
+    int     10h
+
+    ; convert ascii read to an integer
+
+    mov     di, nhts + 6
+    mov     si, in_buffer
+    call    atoi
+
+    ; prepare writing buffer
+
+    mov     si, string
+    call    fill_write_buffer
+
+    ; write to the floppy
+
+    mov     ax, 0
+	mov     es, ax
+    mov     bx, write_buffer
+
+    mov     ah, 03h
+    mov     al, 1
+    mov     ch, [nhts + 4]
+    mov     cl, [nhts + 6]
+    mov     dh, [nhts + 2]
+    mov     dl, 0
+
+    int     13h
+    jc      _error
 
     jmp     _terminate
 
-read_input:
+read_in:
+    mov     si, in_buffer
+
     typing:
         mov     ah, 00h
         int     16h
 
         cmp     al, 08h
-	    je      handle_backspace
+	    je      hdl_backspace
 
 	    cmp     al, 0dh
-	    je      handle_enter
+	    je      hdl_enter
 
-        push    si
-        sub     si, di
-        mov     cx, si
-        pop     si
-        cmp     cx, [buffer_size]
+        cmp     si, in_buffer + 256
         je      typing
 
         mov     [si], al
@@ -208,61 +244,189 @@ read_input:
 
 	    jmp     typing
 
-    handle_backspace:
-	    cmp     si, di
+    hdl_backspace:
+	    cmp     si, in_buffer
 	    je      typing
 
 	    dec     si
     	mov     byte [si], 0
 
         mov     ah, 03h
-	    mov     bh, 0
+        mov     bh, 0
 	    int     10h
 
 	    cmp     dl, 0
-        je      previous_line
+        je      prev_line
 
         mov     ah, 02h
         dec     dl
         int     10h
 
-        mov     ah, 0eh
+        mov     ah, 0ah
         mov     al, 20h
         int     10h
-
-        mov     AH, 02h
-	    int     10h
 
 	    jmp     typing
 
-    previous_line:
+    prev_line:
         mov     ah, 02h
-        mov     dl, 79
         dec     dh
+        mov     dl, 79
         int     10h
 
-        mov     ah, 0eh
+        mov     ah, 0ah
         mov     al, 20h
-        int     10h
-    
-        mov     ah, 02h
         int     10h
     
         jmp     typing
 
-    handle_enter:
-        cmp     si, di
+    hdl_enter:
+        cmp     si, in_buffer
         je      typing
-        
+
+        mov     byte [si], 0
+
         ret
 
-print_read_debug:
+atoi:
+    conv_loop:
+        cmp     byte [si], 0
+        je      conv_done
+
+        mov     ax, [si]
+        sub     ax, '0'
+
+        mov     bx, [di]
+        imul    bx, 10
+        add     bx, ax
+        mov     [di], bx
+
+        inc     si
+
+        jmp     conv_loop
+
+    conv_done:
+        ret
+
+fill_write_buffer:
+    push    si
+    mov     cx, 0
+
+    find_end:
+        cmp     byte [si], 0
+        je      end_found
+
+        inc     si
+        inc     cx
+
+        jmp     find_end
+
+    end_found:
+        pop     si
+        mov     di, write_buffer
+        movzx   bx, [nhts]
+
+    copy_string_to_buffer_loop:
+        push    cx
+        push    si
+        rep     movsb
+
+        pop     si
+        pop     cx
+        dec     bx
+
+        cmp     bx, 0
+        jg      copy_string_to_buffer_loop
+
+    zeros:
+        push    di
+        sub     di, write_buffer
+
+        mov     cx, di
+        pop     di
+
+        cmp     cx, 512
+        je      return
+
+        mov     byte [di], 30h
+        inc     di
+
+        jmp     zeros
+
+    return:
+        ret
+
+_error:
+    push    52h
+    push    52h
+    push    45h
+    mov     cx, 3
+
+    print_err_loop:
+        mov     ah, 0eh
+        pop     bx
+        mov     al, bl
+        int     10h
+
+        dec     cx
+        jnz     print_err_loop
+
+    jmp _terminate
+
+_terminate:
+    mov     ah, 03h
+    mov     bh, 0
+    int     10h
+
+    mov     ah, 02h
+    inc     dh
+    mov     dl, 0
+    int     10h
+
+    jmp     _start
+
+conv_check:
     mov     ah, 0eh
     mov     al, 20h
     int     10h
 
     mov     ah, 0eh
-    mov     al, 65h
+    mov     al, 3eh
+    int     10h
+
+    mov     ah, 0eh
+    mov     al, 3eh
+    int     10h
+
+    mov     ah, 0eh
+    mov     al, 20h
+    int     10h
+
+    mov     ah, 03h
+    int     10h
+
+    push    word [di]
+    
+    mov     ah, 0eh
+    add     word [di], 38
+    mov     al, [di]
+    int     10h
+
+    pop     word [di]
+
+    ret
+
+print_in_buff:
+    mov     ah, 0eh
+    mov     al, 20h
+    int     10h
+
+    mov     ah, 0eh
+    mov     al, 3eh
+    int     10h
+
+    mov     ah, 0eh
+    mov     al, 3eh
     int     10h
 
     mov     ah, 0eh
@@ -275,10 +439,10 @@ print_read_debug:
  
 	mov     ax, 0
     mov     es, ax
-    mov     bp, di
+    mov     bp, in_buffer
 
-	mov     bl, 07h
-    sub     si, di
+    mov     bl, 07h
+    sub     si, in_buffer
     mov     cx, si
 
     mov     ax, 1301h
@@ -286,55 +450,17 @@ print_read_debug:
 
     ret
 
-_error:
-;     mov     ah, 03h
-;     mov     bh, 0
-;     int     10h
-; 
-;     mov     ah, 02h
-;     inc     dh
-;     mov     dl, 0
-;     int     10h
-; 
-;     push    52h
-;     push    4fh
-;     push    52h
-;     push    52h
-;     push    45h
-;     mov     cx, 5
-; 
-;     print_error_loop:
-;         mov     ah, 0eh
-;         pop     bx
-;         mov     al, bl
-;         int     10h
-; 
-;         dec     cx
-;         jnz     print_error_loop
-; 
-    jmp _terminate
+section .data
+    opt_str             dd "1. KBD-->FLP | 2. FLP-->RAM | 3. RAM-->FLP"
+    opt_len             equ 42
 
-atoi:
-    convert_loop:
-        mov     ax, [si]
+    in_awaits_str       dd "STRING = N = {H, T, S} (one value per line)"
+    in_await_len1       equ 9
+    in_await_len2       equ 4
+    in_await_len3       equ 30
 
-        cmp     ax, 0
-        je      convert_done
-
-        sub     ax, '0'
-
-        mov     bx, [di]
-        imul    bx, 10
-        add     bx, ax
-        mov     [di], bx
-
-        inc     si
-
-        jmp     convert_loop
-
-    convert_done:
-        ret
-
-_terminate:
-    mov     ah, 00h
-    int     21h
+section .bss
+    write_buffer    resb 512
+    in_buffer       resb 256
+    string          resb 256
+    nhts            resb 8
---
+++ code/L3-disk-io/task2_boot.asm
@@ -0,0 +1,21 @@
+org 7c00h
+
+mov     ah, 00
+int     13h
+
+mov     ax, 0000h
+mov     es, ax
+mov     bx, 1000h
+
+mov     ah, 02h
+mov     al, 3
+mov     ch, 0
+mov     cl, 2
+mov     dh, 0
+mov     dl, 0
+int     0x13
+
+jmp     0000h:1000h
+
+times 510-($-$$) db 0
+dw 0AA55h
